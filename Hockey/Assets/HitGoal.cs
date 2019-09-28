185fe7786a318c39b563c378e9ec80ceaabca62f
Neilkunal Panchal <neilkunal.panchal@shell.com>
2019-09-28 06:06:31 -0500

    Glide Hockey

---
+++ Hockey/Assets/HitGoal.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class HitGoal : MonoBehaviour
+{
+    public GameObject regionObject;
+    public int lastAgentHit;
+
+    private HockeyRegion region;
+    private PlayerAgent agentA;
+    private PlayerAgent agentB;
+
+    // Use this for initialization
+    void Start()
+    {
+        region = regionObject.GetComponent<HockeyRegion>();
+        agentA = region.agentA.GetComponent<PlayerAgent>();
+        agentB = region.agentB.GetComponent<PlayerAgent>();
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+            if (collision.gameObject.name == "Goal1")
+            {
+                if (lastAgentHit == 0)
+                {
+                    agentA.AddReward(-0.01f);
+                    agentB.SetReward(0);
+                    agentB.score += 1;
+                    Debug.Log("Score Incremented");
+                }
+                else
+                {
+                    agentA.SetReward(0);
+                    agentB.AddReward(-0.01f);
+                    agentA.score += 1;
+                    Debug.Log("Score Incremented");
+                }
+            }
+            else if (collision.gameObject.name == "Goal2")
+            {
+                if (lastAgentHit == 0)
+                {
+                    agentA.AddReward(-0.01f);
+                    agentB.SetReward(0);
+                    agentB.score += 1;
+                    Debug.Log("Score Incremented");
+                }
+                else
+                {
+                    agentA.SetReward(0);
+                    agentB.AddReward(-0.01f);
+                    agentA.score += 1;
+                    Debug.Log("Score Incremented");
+                }
+            }
+
+
+            agentA.Done();
+            agentB.Done();
+            region.MatchReset();
+            Debug.Log(agentA.score);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lastAgentHit = collision.gameObject.name == "Player1" ? 0 : 1;
+        }
+    }
+}
---
+++ Hockey/Assets/PlayerAgent.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
+
+
+public class PlayerAgent : Agent
+{
+    public GameObject puck;
+    public bool invertX;
+    public int score;
+    public GameObject myRegion;
+
+    private Text textComponent;
+    private Rigidbody agentRb;
+    private Rigidbody puckRb;
+    private float invertMult;
+
+    // Looks for the scoreboard based on the name of the gameObjects.
+    // Do not modify the names of the Score GameObjects
+    private const string CanvasName = "Canvas";
+    private const string ScoreBoardAName = "ScoreA";
+    private const string ScoreBoardBName = "ScoreB";
+
+    public override void InitializeAgent()
+    {
+        agentRb = GetComponent<Rigidbody>();
+        puckRb = puck.GetComponent<Rigidbody>();
+        var canvas = GameObject.Find(CanvasName);
+        GameObject scoreBoard;
+        if (invertX)
+        {
+            scoreBoard = canvas.transform.Find(ScoreBoardBName).gameObject;
+
+        }
+        else
+        {
+            scoreBoard = canvas.transform.Find(ScoreBoardAName).gameObject;
+        }
+        textComponent = scoreBoard.GetComponent<Text>();
+
+    }
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(invertMult * (transform.position.x - myRegion.transform.position.x));
+        AddVectorObs(transform.position.z - myRegion.transform.position.z);
+        AddVectorObs(invertMult * agentRb.velocity.x);
+        AddVectorObs(agentRb.velocity.z);
+
+        AddVectorObs(invertMult * (puck.transform.position.x - myRegion.transform.position.x));
+        AddVectorObs(puck.transform.position.z - myRegion.transform.position.z);
+        AddVectorObs(invertMult * puckRb.velocity.x);
+        AddVectorObs(puckRb.velocity.z);
+    }
+
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        var moveX = Mathf.Clamp(vectorAction[0], -1f, 1f) * invertMult;
+        var moveZ = Mathf.Clamp(vectorAction[1], -1f, 1f);
+
+            //if (moveY > 0.5 && transform.position.y - transform.parent.transform.position.y < -1.5f)
+            //{
+            //    agentRb.velocity = new Vector3(agentRb.velocity.x, 2f, 0f);
+            //}
+
+        agentRb.velocity = new Vector3(moveX * 3f, 0f, moveZ*3f);
+
+        if (!invertX && transform.position.x - transform.parent.transform.position.x < -5.0f*invertMult ||
+            invertX && transform.position.x - transform.parent.transform.position.x > 5.0f*invertMult)
+        {
+            transform.position = new Vector3(-invertMult + transform.parent.transform.position.x,
+                                                        transform.position.y,
+                                                        transform.position.z);
+        }
+
+        textComponent.text = score.ToString();
+    }
+
+    public override void AgentReset()
+    {
+        invertMult = invertX ? -1f : 1f;
+
+        transform.position = new Vector3(-invertMult * Random.Range(-2f, -4f), 0.3f, 0f); 
+        //+ transform.parent.transform.position;
+        agentRb.velocity = new Vector3(0f, 0f, 0f);
+    }
+}
